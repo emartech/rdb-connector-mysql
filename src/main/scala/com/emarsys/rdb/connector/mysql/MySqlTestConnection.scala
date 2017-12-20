ed32f07a9b575667fda1574270631a999163cd9f
bkiss <balazs.kiss@emarsys.com>
2017-12-20 11:16:45 +0100

    [fix] test connection uses select 1 now

--- src/main/scala/com/emarsys/rdb/connector/mysql/MySqlTestConnection.scala
+++ src/main/scala/com/emarsys/rdb/connector/mysql/MySqlTestConnection.scala
@@ -1,13 +1,16 @@
 package com.emarsys.rdb.connector.mysql
 
 import com.emarsys.rdb.connector.common.ConnectorResponse
-
-import scala.concurrent.Future
+import com.emarsys.rdb.connector.common.models.Errors.ErrorWithMessage
+import slick.jdbc.MySQLProfile.api._
 
 trait MySqlTestConnection {
   self: MySqlConnector =>
 
   override def testConnection(): ConnectorResponse[Unit] = {
-    Future.successful(Right())
+    db.run(sql"SELECT 1".as[Int]).map(_ => Right(()))
+      .recover {
+        case ex => Left(ErrorWithMessage(ex.toString))
+      }
   }
 }
