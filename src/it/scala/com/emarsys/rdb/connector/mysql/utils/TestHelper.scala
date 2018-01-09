3dc530ef77d5d0856a2eedbc9c9e935c4504bfea
Miklos Martin <miklos.martin@gmail.com>
2018-01-09 12:15:49 +0100

    move db config to application.conf

--- src/it/scala/com/emarsys/rdb/connector/mysql/utils/TestHelper.scala
+++ src/it/scala/com/emarsys/rdb/connector/mysql/utils/TestHelper.scala
@@ -3,43 +3,23 @@ package com.emarsys.rdb.connector.mysql.utils
 import java.util.Properties
 
 import com.emarsys.rdb.connector.mysql.MySqlConnector.{MySqlConnectionConfig, createUrl}
-import com.typesafe.config.ConfigFactory
 import slick.jdbc.MySQLProfile.api._
 import slick.util.AsyncExecutor
 
 import scala.concurrent.Future
 
 object TestHelper {
-  val config = ConfigFactory.load()
-  def configValue(path: String, default: => String): String =
-    if (config.hasPath(path)) config.getString(path)
-    else default
+  import com.typesafe.config.ConfigFactory
+  lazy val config = ConfigFactory.load()
 
-  val TEST_CONNECTION_CONFIG = MySqlConnectionConfig(
-    host = configValue("test.db.host", "db"),
-    port = 3306,
-    dbName = "it-test-db",
-    dbUser = "it-test-user",
-    dbPassword = "it-test-pw",
-    certificate = """-----BEGIN CERTIFICATE-----
-                    |MIIC8DCCAdgCAQIwDQYJKoZIhvcNAQELBQAwPDE6MDgGA1UEAwwxTXlTUUxfU2Vy
-                    |dmVyXzUuNy4xN19BdXRvX0dlbmVyYXRlZF9DQV9DZXJ0aWZpY2F0ZTAeFw0xNzEw
-                    |MjYwNzQ0NTZaFw0yNzEwMjQwNzQ0NTZaMEAxPjA8BgNVBAMMNU15U1FMX1NlcnZl
-                    |cl81LjcuMTdfQXV0b19HZW5lcmF0ZWRfU2VydmVyX0NlcnRpZmljYXRlMIIBIjAN
-                    |BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyiGbFOyftkc/GLr6zRYwinblixgU
-                    |EsWeIVP+Q5xv6Of5Q1d4gGhETzbdb0S3IuwWIcAUeY/yLa6XZs2V3b5BiD6OsNA9
-                    |0r3xAg8IN9EIbrjgiuyPrgRL6TGyTjHA9qkk/rymMR+Nv7a1TeI8L4owA7adYWy+
-                    |Ep4VG3ITRrL/p2KEuWxtOPjdudsV4TqwkZO1EXF59Q0E/3QirKatihkOIBggsKIH
-                    |17c5UQBjkObxUd9XmxeDWorH8wfaaXspzG5tD3eFBsE9SUXSxAfJJcTFB9kyFtgq
-                    |wYe9m62RVR+CX25nuGvVpx39zo+yHdLxZQJmiAO6ZCIuVxA1y1HRWVQ3uQIDAQAB
-                    |MA0GCSqGSIb3DQEBCwUAA4IBAQBZPCh5T4osUKDWD+7v3OTrUBsc1kXLabSXoBYc
-                    |SfxNQvIUwJE9EotNOUWgM6c4zyYLj8AxYPj0vOUAefFE5GVRymp/L38PAHedeoX5
-                    |+HJdkqhKkiOQDJfvJJv7FYfqg5BsraBC6Hx7LUYIyv2Lgz2gSTUDD5k2a2/FTitF
-                    |sD9Ylr9JrNkD5a60ECMej5pGC4Ubzafs4NweTsAsRQVTGQzwK78nUgYu3Vxw3Njv
-                    |ZSe7fLr8jk88uQ9E93Q0Z4xyvGsA9NzRpnG6W0RvY2HgPdt++oWljRyFOEAmwtS6
-                    |91dGnLTCQLp8kgY3Wj7QLYw5ljAmax8EA61tvVvkZsDGnf5w
-                    |-----END CERTIFICATE-----""".stripMargin,
-    connectionParams = ""
+  lazy val TEST_CONNECTION_CONFIG = MySqlConnectionConfig(
+    host = config.getString("dbconf.host"),
+    port= config.getInt("dbconf.port"),
+    dbName= config.getString("dbconf.dbName"),
+    dbUser= config.getString("dbconf.user"),
+    dbPassword= config.getString("dbconf.password"),
+    certificate= config.getString("dbconf.certificate"),
+    connectionParams= config.getString("dbconf.connectionParams")
   )
 
   private lazy val executor = AsyncExecutor.default()
