7b140b29973740aca9f598d3957fad45453ca41b
Robert Doczi <rdoczi@emarsys.com>
2018-01-29 12:44:51 +0100

    [validate-projected-select] add validate projected raw select

--- src/it/scala/com/emarsys/rdb/connector/mysql/MySqlRawSelectItSpec.scala
+++ src/it/scala/com/emarsys/rdb/connector/mysql/MySqlRawSelectItSpec.scala
@@ -3,11 +3,12 @@ package com.emarsys.rdb.connector.mysql
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.testkit.TestKit
+import com.emarsys.rdb.connector.common.models.Errors.ErrorWithMessage
 import com.emarsys.rdb.connector.mysql.utils.SelectDbInitHelper
 import com.emarsys.rdb.connector.test.RawSelectItSpec
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{Await, ExecutionContextExecutor}
 
 class MySqlRawSelectItSpec extends TestKit(ActorSystem()) with RawSelectItSpec with SelectDbInitHelper with WordSpecLike with Matchers with BeforeAndAfterAll{
 
@@ -28,6 +29,20 @@ class MySqlRawSelectItSpec extends TestKit(ActorSystem()) with RawSelectItSpec w
   override val simpleSelect = s"SELECT * FROM `$aTableName`;"
   override val badSimpleSelect = s"SELECT * ForM `$aTableName`"
   override val simpleSelectNoSemicolon = s"""SELECT * FROM `$aTableName`"""
+
+  "#validateProjectedRawSelect" should {
+    "return ok if ok" in {
+      Await.result(connector.validateProjectedRawSelect(simpleSelect, Seq("A1")), awaitTimeout) shouldBe Right()
+    }
+
+    "return ok if no ; in query" in {
+      Await.result(connector.validateProjectedRawSelect(simpleSelectNoSemicolon, Seq("A1")), awaitTimeout) shouldBe Right()
+    }
+
+    "return error if not ok" in {
+      Await.result(connector.validateProjectedRawSelect(simpleSelect, Seq("NONEXISTENT_COLUMN")), awaitTimeout) shouldBe a[Left[ErrorWithMessage, Unit]]
+    }
+  }
 
   "#analyzeRawSelect" should {
     "return result" in {
