edae41300940a39a0f6199b7d3ce152e68b1f7aa
ksisu <kristof.horvath@emarsys.com>
2018-03-06 16:14:33 +0100

    [fix] simple select distinct type

--- src/test/scala/com/emarsys/rdb/connector/mysql/MySqlWritersSpec.scala
+++ src/test/scala/com/emarsys/rdb/connector/mysql/MySqlWritersSpec.scala
@@ -18,7 +18,7 @@ class MySqlWritersSpec extends WordSpecLike with Matchers {
           table = TableName("TABLE1"),
           where = Some(And(Seq(IsNull(FieldName("FIELD1")), And(Seq(IsNull(FieldName("FIELD2")), EqualToValue(FieldName("FIELD3"), Value("VALUE3"))))))),
           limit = Some(100),
-          distinct = true
+          distinct = Some(true)
         )
 
         select.toSql shouldEqual """SELECT DISTINCT `FI\`E'L\\D1`,`FIELD2`,`FIELD3` FROM `TABLE1` WHERE (`FIELD1` IS NULL AND (`FIELD2` IS NULL AND `FIELD3`='VALUE3')) LIMIT 100"""
